ba2608aaeb6185071c33b2d948f80716a0e2dd48
Jwilliamss <jidewilliamss@gmail.com>
2020-10-06 23:33:52 +0000

    Wk2

---
+++ Wk2.py
@@ -0,0 +1,36 @@
+# name = 100
+# print(type(name))
+
+# name = "Mike"
+# print(type(name))
+
+# name = 'John "cool" Miles'
+# print(name)
+
+
+# name = True
+# print(type(name))
+
+# list = [1,2,3,4]
+# list.append = [5]
+# print(type(list))
+# print (list)
+
+
+dict = [{"name": "john Doe",
+"age" : 21,
+"net worth": 5213.4},
+
+{
+    "name":"Micheal moore",
+    "age":"31",
+    "net worth": 20012
+
+}]
+
+print(type(dict))
+print (dict)
+print (dict[1]["net worth"])
+
+
+
